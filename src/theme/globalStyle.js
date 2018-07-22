11859f180b215b723e497c1db6ab3aeab97b9483
Jan Carlo Viray <jviray@bluecedar.com>
2018-07-22 10:06:25 -0700

    import fonts

--- src/theme/globalStyle.js
+++ src/theme/globalStyle.js
@@ -1,8 +1,25 @@
 import { injectGlobal } from 'styled-components'
+
 injectGlobal`
+  @import url(‘https://fonts.googleapis.com/css?family=Montserrat|Roboto');
+
   body {
     padding: 0;
     margin: 20px !important;
-    font-family: sans-serif;
+    font-family: Roboto, sans-serif;
+  }
+
+  h1 {
+    font-family: Montserrat;
   }
 `
+
+/*
+import Montserrat from ‘./fonts/Montserrat-Regular.ttf’
+injectGlobal`
+  @font-face {
+    font-family: Montserrat;
+    src: url(${Montserrat});
+  }
+`
+*/
