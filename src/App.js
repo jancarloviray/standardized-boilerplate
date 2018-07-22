0b64c0074695612f03435d3f6e99527934776b1f
Jan Carlo Viray <jviray@bluecedar.com>
2018-07-22 10:00:43 -0700

    using styled-components for layout

--- src/App.js
+++ src/App.js
@@ -2,43 +2,39 @@ import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { Button } from 'reactstrap';
 import logo from './logo.svg';
+import styled from 'styled-components'
 import './App.css';
+
+const AppWrapper = styled.div`
+  text-align: center;
+`
 
 const App = () => (
   <Router>
-    <div>
-      <img src={logo} className="App-logo" alt="logo" />
-      <Button>Hello</Button>
+    <AppWrapper>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to React</h1>
+        <Button>Hello</Button>
+      </header>
       <ul>
         <li>
           <Link to="/">Home</Link>
         </li>
         <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
           <Link to="/topics">Topics</Link>
         </li>
       </ul>
-
       <hr />
-
       <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
       <Route path="/topics" component={Topics} />
-    </div>
+    </AppWrapper>
   </Router>
 );
 
 const Home = () => (
   <div>
     <h2>Home</h2>
-  </div>
-);
-
-const About = () => (
-  <div>
-    <h2>About</h2>
   </div>
 );
 
